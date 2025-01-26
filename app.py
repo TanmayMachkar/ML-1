e1f5381be0d44ae6f61543765d4f3580d3b14771
TanmaySpamzzz <tanmaymacho@gmail.com>
2025-01-26 18:19:20 +0530

    updated requirements.txt for deployment

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 regmodel = pickle.load(open("regmodel.pkl", "rb"))
 scalar = pickle.load(open("scaling.pkl", "rb"))
 
